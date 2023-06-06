a4b318c6778dc46487811fb11d3c390a8ce94f0d
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2023-06-06 09:36:30 -0300

    primeros componentes

---
+++ src/redux/reducerApp.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    count: 0
+}
+
+const reducerApp = (state = initialState, action) => {
+    switch (action.type) {
+      
+        default:
+            return state;
+    }
+}
