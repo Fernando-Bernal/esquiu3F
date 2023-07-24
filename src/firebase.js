1c30b8bc8b3f372c1aa2a3585542d8f3c9beda15
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2023-07-24 11:34:52 -0300

    protection

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBeVPO_KRUTeSzAsrky0lFfpgJfvq1UYQ0",
-	authDomain: "esquiu3-66e8a.firebaseapp.com",
-	projectId: "esquiu3-66e8a",
-	storageBucket: "esquiu3-66e8a.appspot.com",
-	messagingSenderId: "330337098288",
-	appId: "1:330337098288:web:b9a1ce803e5833625b6bf5",
-	measurementId: "G-VP99KRMVJC",
+	apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
+	authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+	storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+	measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID,
 };
 
 initializeApp(firebaseConfig);
