2bdd5b40ef353f48280314671f96c54e603cca72
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2023-08-22 00:18:43 -0300

    Quienes somos

--- src/componentes/QuienesSomos.jsx
+++ src/componentes/QuienesSomos.jsx
@@ -13,37 +13,37 @@ function QuienesSomos() {
 				<p>
 					Bienvenidos a nuestro apasionante proyecto, donde la familia, el
 					fútbol y la calidad se unen en un solo lugar.
-                    <br /> 
-                    Somos un equipo	comprometido con el deporte y nuestro objetivo es proporcionar
-					experiencias deportivas inigualables que fomenten la unión familiar y
-					la pasión por el fútbol. 
-                    <br />
-                    El corazón de nuestro proyecto es el torneo
-					SQ3, una competición de fútbol 11 masculino con 6 categorías
-					emocionantes. Queremos que cada jugador se desafíe y alcance su máximo
-					potencial, mientras que la competencia se combina con el espíritu
-					deportivo y la camaradería. 
-                    <br />
-                    Pero no nos detenemos ahí. Nuestro Campus
-					ofrece instalaciones de primer nivel, incluyendo canchas de calidad
-					que garantizan una experiencia de juego excepcional. 
-                    <br />
-                    Además, el Esquiú Day es una fecha especial en la que celebramos el fútbol y el espíritu
-					comunitario. Es un día lleno de diversión, partidos amistosos y
-					actividades para todas las edades, creando lazos y recuerdos
-					imborrables. 
-                    <br />En nuestro proyecto, la calidad es primordial. No solo
-					ofrecemos canchas de última generación, sino también un equipo
-					profesional que se esfuerza por brindar una experiencia excepcional en
-					cada evento. 
-                    <br />Somos más que un proyecto deportivo, somos una familia
-					unida por el amor al fútbol y el deseo de crear momentos inolvidables.
+					<br />
+					Somos un equipo comprometido con el deporte y nuestro objetivo es
+					proporcionar experiencias deportivas inigualables que fomenten la
+					unión familiar y la pasión por el fútbol.
+					<br />
+					El corazón de nuestro proyecto es el torneo SQ3, una competición de
+					fútbol 11 masculino con 6 categorías emocionantes. Queremos que cada
+					jugador se desafíe y alcance su máximo potencial, mientras que la
+					competencia se combina con el espíritu deportivo y la camaradería.
+					<br />
+					Pero no nos detenemos ahí. Nuestro Campus ofrece instalaciones de
+					primer nivel, incluyendo canchas de calidad que garantizan una
+					experiencia de juego excepcional.
+					<br />
+					Además, el Esquiú Day es una fecha especial en la que celebramos el
+					fútbol y el espíritu comunitario. Es un día lleno de diversión,
+					partidos amistosos y actividades para todas las edades, creando lazos
+					y recuerdos imborrables.
+					<br />
+					En nuestro proyecto, la calidad es primordial. No solo ofrecemos
+					canchas de última generación, sino también un equipo profesional que
+					se esfuerza por brindar una experiencia excepcional en cada evento.
+					<br />
+					Somos más que un proyecto deportivo, somos una familia unida por el
+					amor al fútbol y el deseo de crear momentos inolvidables.
+					<br />
 					Bienvenidos a Esquiú 3.
 				</p>
-                <Img src={arquero} alt="foto" />
+				<Img src={arquero} alt="foto" />
 			</Section>
-            <div>
-            </div>
+			<div></div>
 			<Footer />
 		</Container>
 	);
@@ -69,26 +69,25 @@ const Container = styled.div`
 const Section = styled.section`
 	width: 75%;
 	margin: 20px auto;
-    padding: 20px;
-    text-align: left;
-    font-size: 1.2rem;
-    line-height: 1.5;
-    font-family: "Roboto", sans-serif;
-    font-weight: 300;
-    position: relative;
+	padding: 20px;
+	text-align: left;
+	font-size: 1.2rem;
+	line-height: 1.5;
+	font-family: "Roboto", sans-serif;
+	font-weight: 300;
+	position: relative;
 	z-index: -10;
 	min-height: 100vh;
 
 	@media (min-width: 768px) {
-        width: 80%;
+		width: 80%;
 	}
 `;
 
 const Img = styled.img`
-    width: 100%;
-    margin: 20px auto;
-    display: block;
-    position: relative;
-    z-index: -10;
-    
+	width: 100%;
+	margin: 20px auto;
+	display: block;
+	position: relative;
+	z-index: -10;
 `;
